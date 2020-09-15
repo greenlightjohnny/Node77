d47953a6c38f20cb7c00f4d7e296a4f47d0d9f83
fj <castelloiv@gmail.com>
2020-09-15 17:33:36 -0400

    adding views, routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,18 @@
 ////Express auth routes, what functions each route should perform.
+
+//// Destructuring the Router object from the express NPM package
 const { Router } = require("express");
+
+///// Get the controller object,  which exports a bunch of different functions
+const authController = require("../controllers/authController");
 
 ///Init new router object, can save as any name you want, but router is most common.
 const router = Router();
 
-router.get("./signup", () => {});
-router.post("./signup", () => {});
-router.get("./login", () => {});
-router.post("./login", () => {});
+///When a route is hit, uses the function imported from the controller to do something cool
+router.get("/signup", authController.signup_get);
+router.post("/signup", authController.signup_post);
+router.get("/login", authController.login_get);
+router.post("/login", authController.login_post);
 
 module.exports = router;
