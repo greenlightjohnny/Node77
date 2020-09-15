5c1c7f9bc341525f13262801aec5e55a2e7f55b7
fj <castelloiv@gmail.com>
2020-09-15 17:43:55 -0400

    Testing routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,22 @@
 // Wrap each function in an individual export
 
 ///Sends the signup view to user
-module.exports.signup_get = (req, rs) => {
+///Express will automatically look in the views directory and show what is inside. So "signup" for the render means express will look for a signup file inside the views directory. In this case it is an EJS file
+module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
 
 ////Sends the login view to user
-module.exports.login_get = (req, rs) => {
+module.exports.login_get = (req, res) => {
   res.render("login");
 };
 
 //////Receives HTTP POST request from client to sign up, sends to database
-module.exports.signup_post = (req, rs) => {
+module.exports.signup_post = (req, res) => {
   res.send("New signup");
 };
 
 //////Receives HTTP POST request from client to login
-module.exports.login_post = (req, rs) => {
+module.exports.login_post = (req, res) => {
   res.send("Logged in");
 };
